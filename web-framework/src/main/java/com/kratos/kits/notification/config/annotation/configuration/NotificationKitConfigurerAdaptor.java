8425438952df8ad8826f585d4540c37294fccdce
herton7362 <he.tang@everydayratings.com>
2017-09-11 16:54:26 +0800

    编写通知组件

--- web-framework/src/main/java/com/kratos/kits/notification/config/annotation/configuration/NotificationKitConfigurerAdaptor.java
+++ web-framework/src/main/java/com/kratos/kits/notification/config/annotation/configuration/NotificationKitConfigurerAdaptor.java
@@ -1,24 +1,10 @@
 package com.kratos.kits.notification.config.annotation.configuration;
 
 import com.kratos.entity.BaseUser;
-import com.kratos.kits.notification.config.annotation.builder.NotificationProviders;
-import com.kratos.kits.notification.config.annotation.builder.NotificationTypes;
 import com.kratos.kits.notification.config.annotation.configurer.SendConfigurer;
 import com.kratos.kits.notification.message.NotificationMessageType;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class NotificationKitConfigurerAdaptor implements NotificationKitConfigurer {
-
-    @Override
-    public void configure(NotificationProviders notificationProvider) throws Exception {
-
-    }
-
-    @Override
-    public void configure(NotificationTypes notificationTypes) throws Exception {
-
-    }
+public abstract class NotificationKitConfigurerAdaptor implements NotificationKitConfigurer {
 
     @Override
     public void configure(SendConfigurer sendConfigurer) throws Exception {
