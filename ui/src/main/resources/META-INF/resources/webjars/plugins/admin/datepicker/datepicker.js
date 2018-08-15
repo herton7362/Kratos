75b91a461c38a221b17b9aab2f25e5e5ba8da090
herton7362 <he.tang@everydayratings.com>
2018-08-15 15:00:35 +0800

    add date type

--- ui/src/main/resources/META-INF/resources/webjars/plugins/admin/datepicker/datepicker.js
+++ ui/src/main/resources/META-INF/resources/webjars/plugins/admin/datepicker/datepicker.js
@@ -1,15 +1,22 @@
 define(['jquery', 'datepicker-cn', 'utils'], function($) {
     return {
         props: {
-            value: [String, Number]
+            value: [String, Number],
+            type: String
         },
         template: '<input type="text" class="form-control" :value="formattedDate">',
         methods: {
+            formatter: function (val) {
+                if(this.type !== 'date') {
+                    return new Date(val).format('yyyy-MM-dd');
+                }
+                return val;
+            }
         },
         watch: {
             value: function(val) {
                 if(val) {
-                    this.formattedDate = new Date(val).format('yyyy-MM-dd');
+                    this.formattedDate = this.formatter(val);
                 } else {
                     this.formattedDate = null;
                 }
@@ -28,7 +35,11 @@ define(['jquery', 'datepicker-cn', 'utils'], function($) {
                 autoclose: true
             });
             $(this.$el).on('change', function() {
-                self.$emit('input', new Date(this.value).getTime());
+                if(self.type !== 'date') {
+                    self.$emit('input', new Date(this.value).getTime());
+                } else {
+                    self.$emit('input', this.value);
+                }
             });
             this.$emit('mounted', this);
         }
